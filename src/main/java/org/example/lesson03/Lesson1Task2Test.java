24c4ce46893e7a3d3e5cfd9680d5b899b4b562bb
Lala <l.ogneva@yahoo.com>
2021-01-15 10:46:41 -0600

    commit2

--- src/main/java/org/example/lesson03/Lesson1Task2Test.java
+++ src/main/java/org/example/lesson03/Lesson1Task2Test.java
@@ -1,26 +1,38 @@
 package org.example.lesson03;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-
 import org.openqa.selenium.chrome.ChromeDriver;
-
 import org.openqa.selenium.Dimension;
 
 public class Lesson1Task2Test {
-    public static void main(String[] args) {
 
+    private WebDriver driver;
+
+    @BeforeClass
+    public static void setupClass() {
         WebDriverManager.chromedriver().setup();
+    }
 
+    @Before
+    public void setupTest() {
+        driver = new ChromeDriver();
+    }
 
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-notifications");
-        options.addArguments("--disable-popup-bloking");
+    @After
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 
-        WebDriver driver = new ChromeDriver(options);
-
+    @Test
+    public void test() {
         driver.get("https://www.diary.ru/");
         driver.manage().window().setSize(new Dimension(1050, 660));
 
@@ -42,4 +54,5 @@ public class Lesson1Task2Test {
 
         driver.quit();
     }
+
 }
